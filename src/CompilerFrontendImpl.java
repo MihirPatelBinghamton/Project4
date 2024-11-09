cceac0a18e35c38a227ce87ac3887cf07790d7a7
Mihir <20mihir01@gmail.com>
2024-11-09 11:30:56 -0500

    prj4

--- src/CompilerFrontendImpl.java
+++ src/CompilerFrontendImpl.java
@@ -7,21 +7,86 @@ public class CompilerFrontendImpl extends CompilerFrontend {
         super(debug_);
     }
 
-    /*
-     * Initializes the local field "lex" to be equal to the desired lexer.
-     * The desired lexer has the following specification:
-     * 
-     * NUM: [0-9]*\.[0-9]+
-     * PLUS: \+
-     * MINUS: -
-     * TIMES: \*
-     * DIV: /
-     * WHITE_SPACE (' '|\n|\r|\t)*
-     */
+
     @Override
     protected void init_lexer() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'init_lexer'");
-    }
+        LexerImpl lexer = new LexerImpl();
+
+        // NUM: [0-9]*\.[0-9]+
+        Automaton numAutomaton = new AutomatonImpl();
+        numAutomaton.addState(0, true, false); // Initial state
+        numAutomaton.addState(1, false, false); // For digits before decimal
+        numAutomaton.addState(2, false, false); // Decimal point state
+        numAutomaton.addState(3, false, true);  // Accepting state for digits after decimal
+        for (char c = '0'; c <= '9'; c++) {
+            numAutomaton.addTransition(0, c, 1); // Initial state to digits before decimal
+            numAutomaton.addTransition(1, c, 1); // Loop for digits before decimal
+            numAutomaton.addTransition(3, c, 3); // Loop for digits after decimal
+        }
+        numAutomaton.addTransition(1, '.', 2); // Transition from digits to decimal
+        for (char c = '0'; c <= '9'; c++) {
+            numAutomaton.addTransition(2, c, 3); // Transition from decimal to digits after
+        }
+        lexer.add_automaton(TokenType.NUM, numAutomaton);
+
+        // PLUS: +
+        Automaton plusAutomaton = new AutomatonImpl();
+        plusAutomaton.addState(0, true, false);
+        plusAutomaton.addState(1, false, true);
+        plusAutomaton.addTransition(0, '+', 1);
+        lexer.add_automaton(TokenType.PLUS, plusAutomaton);
+
+        // MINUS: -
+        Automaton minusAutomaton = new AutomatonImpl();
+        minusAutomaton.addState(0, true, false);
+        minusAutomaton.addState(1, false, true);
+        minusAutomaton.addTransition(0, '-', 1);
+        lexer.add_automaton(TokenType.MINUS, minusAutomaton);
+
+        // TIMES: *
+        Automaton timesAutomaton = new AutomatonImpl();
+        timesAutomaton.addState(0, true, false);
+        timesAutomaton.addState(1, false, true);
+        timesAutomaton.addTransition(0, '*', 1);
+        lexer.add_automaton(TokenType.TIMES, timesAutomaton);
+
+        // DIV: /
+        Automaton divAutomaton = new AutomatonImpl();
+        divAutomaton.addState(0, true, false);
+        divAutomaton.addState(1, false, true);
+        divAutomaton.addTransition(0, '/', 1);
+        lexer.add_automaton(TokenType.DIV, divAutomaton);
+
+        // LPAREN: (
+        Automaton lparenAutomaton = new AutomatonImpl();
+        lparenAutomaton.addState(0, true, false);
+        lparenAutomaton.addState(1, false, true);
+        lparenAutomaton.addTransition(0, '(', 1);
+        lexer.add_automaton(TokenType.LPAREN, lparenAutomaton);
+
+        // RPAREN: )
+        Automaton rparenAutomaton = new AutomatonImpl();
+        rparenAutomaton.addState(0, true, false);
+        rparenAutomaton.addState(1, false, true);
+        rparenAutomaton.addTransition(0, ')', 1);
+        lexer.add_automaton(TokenType.RPAREN, rparenAutomaton);
+
+        // WHITE_SPACE: (' '|\n|\r|\t)*
+        Automaton whitespaceAutomaton = new AutomatonImpl();
+        whitespaceAutomaton.addState(0, true, true);
+        whitespaceAutomaton.addState(1, false, true);
+        whitespaceAutomaton.addTransition(0, ' ', 1);
+        whitespaceAutomaton.addTransition(0, '\n', 1);
+        whitespaceAutomaton.addTransition(0, '\r', 1);
+        whitespaceAutomaton.addTransition(0, '\t', 1);
+        whitespaceAutomaton.addTransition(1, ' ', 1);
+        whitespaceAutomaton.addTransition(1, '\n', 1);
+        whitespaceAutomaton.addTransition(1, '\r', 1);
+        whitespaceAutomaton.addTransition(1, '\t', 1);
+        lexer.add_automaton(TokenType.WHITE_SPACE, whitespaceAutomaton);
+
+        // Assign lexer to the class field
+        this.lex = lexer;
+        }
 
 }
--- src/ParserImpl.java
+++ src/ParserImpl.java
@@ -1,24 +1,82 @@
+import java.text.ParseException;
 
 public class ParserImpl extends Parser {
 
-    /*
-     * Implements a recursive-descent parser for the following CFG:
-     * 
-     * T -> F AddOp T              { if ($2.type == TokenType.PLUS) { $$ = new PlusExpr($1,$3); } else { $$ = new MinusExpr($1, $3); } }
-     * T -> F                      { $$ = $1; }
-     * F -> Lit MulOp F            { if ($2.type == TokenType.Times) { $$ = new TimesExpr($1,$3); } else { $$ = new DivExpr($1, $3); } }
-     * F -> Lit                    { $$ = $1; }
-     * Lit -> NUM                  { $$ = new FloatExpr(Float.parseFloat($1.lexeme)); }
-     * Lit -> LPAREN T RPAREN      { $$ = $2; }
-     * AddOp -> PLUS               { $$ = $1; }
-     * AddOp -> MINUS              { $$ = $1; }
-     * MulOp -> TIMES              { $$ = $1; }
-     * MulOp -> DIV                { $$ = $1; }
-     */
+    
     @Override
     public Expr do_parse() throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'do_parse'");
+        return parseT();
+    }
+     // T -> F AddOp T | F
+    private Expr parseT() throws Exception {
+        Expr left = parseF();
+        
+        if (peek(TokenType.PLUS, 0) || peek(TokenType.MINUS, 0)) {
+            Token op = parseAddOp();
+            Expr right = parseT();
+            
+            if (op.ty == TokenType.PLUS) {
+                return new PlusExpr(left, right);
+            } else {  // Must be MINUS
+                return new MinusExpr(left, right);
+            }
+        }
+        
+        return left;  // T -> F
     }
 
+    // F -> Lit MulOp F | Lit
+    private Expr parseF() throws Exception {
+        Expr left = parseLit();
+        
+        if (peek(TokenType.TIMES, 0) || peek(TokenType.DIV, 0)) {
+            Token op = parseMulOp();
+            Expr right = parseF();
+            
+            if (op.ty == TokenType.TIMES) {
+                return new TimesExpr(left, right);
+            } else {  // Must be DIV
+                return new DivExpr(left, right);
+            }
+        }
+        
+        return left;  // F -> Lit
+    }
+
+    // Lit -> NUM | LPAREN T RPAREN
+    private Expr parseLit() throws Exception {
+        if (peek(TokenType.NUM, 0)) {
+            Token num = consume(TokenType.NUM);
+            return new FloatExpr(Float.parseFloat(num.lexeme));
+        } else if (peek(TokenType.LPAREN, 0)) {
+            consume(TokenType.LPAREN);  // consume '('
+            Expr expr = parseT();
+            consume(TokenType.RPAREN);  // consume ')'
+            return expr;
+        } else {
+            throw new Exception("Expected a number or '('");
+        }
+    }
+
+    // AddOp -> PLUS | MINUS
+    private Token parseAddOp() throws Exception {
+        if (peek(TokenType.PLUS, 0)) {
+            return consume(TokenType.PLUS);
+        } else if (peek(TokenType.MINUS, 0)) {
+            return consume(TokenType.MINUS);
+        } else {
+            throw new Exception("Expected '+' or '-'");
+        }
+    }
+
+    // MulOp -> TIMES | DIV
+    private Token parseMulOp() throws Exception {
+        if (peek(TokenType.TIMES, 0)) {
+            return consume(TokenType.TIMES);
+        } else if (peek(TokenType.DIV, 0)) {
+            return consume(TokenType.DIV);
+        } else {
+            throw new Exception("Expected '*' or '/'");
+        }
+    }
 }
